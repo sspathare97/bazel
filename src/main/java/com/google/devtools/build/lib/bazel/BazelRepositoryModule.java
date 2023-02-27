18d22c38efb6908116a6a0d0aaad2676bf2d63cd
salma-samy <salmasamy@google.com>
2023-02-27 08:53:39 -0800

    Support (workspace) relative paths in --override_module
    PiperOrigin-RevId: 512639734
    Change-Id: I992d68e40b600ba921000d34d878186701baad2c

--- src/main/java/com/google/devtools/build/lib/bazel/BazelRepositoryModule.java
+++ src/main/java/com/google/devtools/build/lib/bazel/BazelRepositoryModule.java
@@ -394,7 +394,9 @@ public class BazelRepositoryModule extends BlazeModule {
         // We use a LinkedHashMap to preserve the iteration order.
         Map<RepositoryName, PathFragment> overrideMap = new LinkedHashMap<>();
         for (RepositoryOverride override : repoOptions.repositoryOverrides) {
-          overrideMap.put(override.repositoryName(), override.path());
+          String repoPath =
+              override.path().replace("%workspace%", env.getWorkspace().getPathString());
+          overrideMap.put(override.repositoryName(), PathFragment.create(repoPath));
         }
         ImmutableMap<RepositoryName, PathFragment> newOverrides = ImmutableMap.copyOf(overrideMap);
         if (!Maps.difference(overrides, newOverrides).areEqual()) {
@@ -406,9 +408,10 @@ public class BazelRepositoryModule extends BlazeModule {
 
       if (repoOptions.moduleOverrides != null) {
         Map<String, ModuleOverride> moduleOverrideMap = new LinkedHashMap<>();
-        for (RepositoryOptions.ModuleOverride modOverride : repoOptions.moduleOverrides) {
-          moduleOverrideMap.put(
-              modOverride.moduleName(), LocalPathOverride.create(modOverride.path()));
+        for (RepositoryOptions.ModuleOverride override : repoOptions.moduleOverrides) {
+          String modulePath =
+              override.path().replace("%workspace%", env.getWorkspace().getPathString());
+          moduleOverrideMap.put(override.moduleName(), LocalPathOverride.create(modulePath));
         }
         ImmutableMap<String, ModuleOverride> newModOverrides =
             ImmutableMap.copyOf(moduleOverrideMap);
